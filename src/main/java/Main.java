7319cc43032f909f4629d78d0b8b4b7d4302e4d0
Ulas Yakut <uyakut@Zipcoders-MacBook-Pro-5.local>
2024-10-27 08:53:21 -0400

    changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,41 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.Scanner;
+
+
 public class Main {
 
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+
+        int random = ThreadLocalRandom.current().nextInt(20, 81);
+        int num;
+        //System.out.println(random);
+        Scanner in = new Scanner(System.in);
+        System.out.print("Type your number: ");
+        num = in.nextInt();
+        //System.out.println(num);
+
+
+        //do while statement needed
+        if (random == num) {
+                System.out.println("Your guess is correct!");
+        } else if (num > random) {
+                System.out.println("Your guess is too large!");
+        } else {
+                System.out.println("Your guess is too small!");
+        }
+
+
     }
+
+
+
 }
+
+
+
+
